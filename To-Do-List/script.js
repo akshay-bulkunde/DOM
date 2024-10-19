157c52b914f1cb894ba768e204047eb2a2f6ebc1
akshay-bulkunde <akshay.bulkunde.29.3@mountblue.tech>
2024-10-19 10:42:11 +0530

    Implement ToDo List

--- To-Do-List/script.js
+++ To-Do-List/script.js
@@ -1,12 +1,47 @@
 const inputField = document.querySelector('input')
-const container = document.querySelector('.container')
-const body = document.querySelector('body')
+const unorderList = document.querySelector('ul')
+
 inputField.addEventListener('keydown', function (event) {
     if (event.key == 'Enter') {
         const listItem = inputField.value;
         const newList = document.createElement('li');
-        newList.innerText = listItem;
-        container.appendChild(newList)
+        const textField = document.createElement('input')
+        textField.setAttribute('readonly' , true)
+        textField.setAttribute('value', listItem)
+
+        // Pencil
+        const pencil = document.createElement('i');
+        pencil.classList.add('fa-solid');
+        pencil.classList.add('fa-pencil');
+
+        // Delete
+        const trash = document.createElement('i');
+        trash.classList.add('fa-solid');
+        trash.classList.add('fa-trash');
+
+        newList.appendChild(textField);
+        newList.appendChild(pencil);
+        newList.appendChild(trash);
+        unorderList.appendChild(newList)
         inputField.value = '';
     }
 })
+
+
+unorderList.addEventListener('click', function (event) {
+    if (event.target.classList.contains('fa-pencil')) {
+        event.target.classList.remove('fa-pencil');
+        event.target.classList.add('fa-floppy-disk');
+        event.target.closest('li').querySelector('input').removeAttribute('readonly')
+    }
+
+    else if (event.target.classList.contains('fa-floppy-disk')) {
+        event.target.classList.add('fa-pencil');
+        event.target.classList.remove('fa-floppy-disk');
+    }
+
+    else if (event.target.classList.contains('fa-trash')) {
+        event.target.parentElement.remove();
+    }
+})
+
